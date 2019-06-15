0671034051a453207c77c094c3afaa16f8f88d07
Murat Yigit <muratyigit@Murats-MBP.router>
2019-06-15 15:59:47 -0400

    Set up production webpack build

--- src/app.js
+++ src/app.js
@@ -11,15 +11,7 @@ import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
-
-const waterBill = store.dispatch(addExpense({ description: 'Water Bill', amount: 4500 }));
-const gasBill = store.dispatch(addExpense({ description: 'Gas Bill', amount: 0, createdAt: 1000 }));
-const rentBill = store.dispatch(addExpense({ description: 'Rent', amount: 109500 }));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-
+console.log('test');
 const jsx = (
 	<Provider store={store}>
 		<AppRouter />
