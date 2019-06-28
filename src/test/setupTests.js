c1321ba3a3aa084fc292922c5d031967f9026efc
Murat Yigit <muratyigit@Murats-MBP.router>
2019-06-27 20:57:07 -0400

    Setup test database env

--- src/test/setupTests.js
+++ src/test/setupTests.js
@@ -1,6 +1,9 @@
 //This file is going to help us to configure the environment we are running in
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import DotEnv from 'dotenv';
+
+DotEnv.config({ path: '.env.test' });
 
 Enzyme.configure({
 	adapter: new Adapter()
