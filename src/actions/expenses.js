c985c49fcfa9a947ff759b399b5b0028f7eb699f
Murat Yigit <muratyigit@Murats-MBP.router>
2019-06-29 15:03:54 -0400

    Set up startEditExpense

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -33,7 +33,7 @@ export const removeExpense = ({ id } = {}) => ({
 //START REMOVE EXPENSE
 export const startRemoveExpense = ({ id } = {}) => {
 	return (dispatch) => {
-		return database.ref(`expenses/${id}`).remove().then((ref) => {
+		return database.ref(`expenses/${id}`).remove().then(() => {
 			dispatch(removeExpense({ id }));
 		});
 	};
@@ -45,6 +45,14 @@ export const editExpense = (id, updates) => ({
 	id,
 	updates
 });
+
+export const startEditExpense = (id, updates) => {
+	return (dispatch) => {
+		return database.ref(`expenses/${id}`).update(updates).then(() => {
+			dispatch(editExpense(id, updates));
+		});
+	};
+};
 
 //SET EXPENSE
 export const setExpenses = (expenses) => ({
--- src/test/actions/expenses.test.js
+++ src/test/actions/expenses.test.js
@@ -5,7 +5,8 @@ import {
 	removeExpense,
 	setExpenses,
 	startSetExpenses,
-	startRemoveExpense
+	startRemoveExpense,
+	startEditExpense
 } from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
 import configureMockStore from 'redux-mock-store';
@@ -56,6 +57,27 @@ test('should edit expense action object', () => {
 		id: '12345',
 		updates: { note: 'Murat yigit' }
 	});
+});
+
+test('should update the expenses on firebase', (done) => {
+	const store = createMockStore();
+	const id = expenses[0].id;
+	const updates = { amount: 12332 };
+	store
+		.dispatch(startEditExpense(id, updates))
+		.then(() => {
+			const actions = store.getActions();
+			expect(actions[0]).toEqual({
+				type: 'EDIT_EXPENSE',
+				id,
+				updates
+			});
+			return database.ref(`expenses/${id}`).once('value');
+		})
+		.then((snapshot) => {
+			expect(snapshot.val().amount).toBe(updates.amount);
+			done();
+		});
 });
 
 test('should setup add expense action object for provided values', () => {
