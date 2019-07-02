baa7e30cb5d1dc97c7ba5a6fc70a841d078a44de
Murat Yigit <muratyigit@Murats-MBP.router>
2019-07-02 16:38:36 -0400

    Adding styles

---
+++ src/components/LoadingPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const LoadingPage = () => (
+	<div className="loaders">
+		<img className="loaders__image" src="/images/loader.gif" />
+	</div>
+);
+
+export default LoadingPage;
